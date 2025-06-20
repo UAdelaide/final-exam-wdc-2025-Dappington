56de9b6749006d6dfa83344f34bcb2efb7069a2c
Dappington <lordappington@gmail.com>
2025-06-20 08:12:28 +0000

    [automatic commit] 2025-06-20 08:12:27 UTC 121.45.82.31

--- part1/dogwalkqueries.sql
+++ part1/dogwalkqueries.sql
@@ -1,5 +1,5 @@
 INSERT INTO Users (username, email, password_hash, role)
-VALUES (alice123, email alice@example.com, password hash hashed123, and role owner)
+VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner')
 
 A user with the username alice123, email alice@example.com, password hash hashed123, and role owner.
 A user with the username bobwalker, email bob@example.com, password hash hashed456, and role walker.

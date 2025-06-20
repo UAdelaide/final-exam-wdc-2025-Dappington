c6effc5adeb7a5ea02ae1097e6d07a0a8c41942d
Dappington <lordappington@gmail.com>
2025-06-20 08:18:25 +0000

    [automatic commit] 2025-06-20 08:18:24 UTC 121.45.82.31

--- part1/dogwalkqueries.sql
+++ part1/dogwalkqueries.sql
@@ -8,7 +8,7 @@ VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
 ('ethanhunt', 'ethan@example.com', '177013', 'owner');
 
 INSERT INTO Dogs (owner_id, name, size)
-VALUES (LAST_INSERT_ID(), )
+VALUES (LAST_INSERT_ID(), 'Max', 'medium')
 
 Five dogs:
 A dog named Max, who is medium-sized and owned by alice123.

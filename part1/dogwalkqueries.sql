9a745acf9d6fcacb6264ddba1c79b279b3a5449c
Dappington <lordappington@gmail.com>
2025-06-20 08:23:59 +0000

    [automatic commit] 2025-06-20 08:23:58 UTC 121.45.82.31

--- part1/dogwalkqueries.sql
+++ part1/dogwalkqueries.sql
@@ -16,13 +16,10 @@ VALUES (LAST_INSERT_ID(), 'Bella', 'small');
 INSERT INTO Users (username, email, password_hash, role)
 VALUES ('bobwalker', 'bob@example.com', 'hashed456', 'walker');
 
+INSERT INTO Users (username, email, password_hash, role)
+VALUES ('navydavie', 'dave@example.com', 'passwrd222', 'walker');
 
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('navydavie', 'dave@example.com', 'passwrd222', 'walker'),
 ('ethanhunt', 'ethan@example.com', '177013', 'owner');
-
-
-
-Five dogs:
-A dog named Bella, who is small and owned by carol123.
 1 more dogs with details of your choosing.

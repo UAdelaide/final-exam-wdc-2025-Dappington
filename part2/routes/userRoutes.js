e96a4573db6bcfa9dc8531f8589534e051411a55
Dappington <lordappington@gmail.com>
2025-06-21 05:07:39 +0000

    [automatic commit] 2025-06-21 05:07:38 UTC 121.45.82.31

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -71,8 +71,8 @@ router.get('/mydogs', async(req, res) => {
     if (!req.session.loggedIn) {
         res.sendStatus(401);
     }
-    const dogs = await db.query(`SELECT dog_id, name FROM Dogs WHERE owner_id = ?;`, [req.session.user_id]);
-    res.send(dogs);
+    {const dogs = await db.query(`SELECT dog_id, name FROM Dogs WHERE owner_id = ?;`, [req.session.user_id]);
+    res.send(dogs);}
 });
 
 router.get('/logout', function(req,res){

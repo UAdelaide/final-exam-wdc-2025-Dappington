41d602feba9be6922614c29a951e3d598fce5e41
Dappington <lordappington@gmail.com>
2025-06-21 05:57:45 +0000

    [automatic commit] 2025-06-21 05:57:43 UTC 121.45.82.31

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -39,6 +39,7 @@ router.post('/', async (req, res) => {
 router.post('/:id/apply', async (req, res) => {
   const requestId = req.params.id;
   const { walker_id } = req.body;
+  console.log()
 
   try {
     await db.query(`
